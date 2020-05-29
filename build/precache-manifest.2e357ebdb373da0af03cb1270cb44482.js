17f88228e9ddd8580648a9a1c91c2aa217f17760
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-05-30 00:41:52 +0800

    add build

---
+++ build/precache-manifest.2e357ebdb373da0af03cb1270cb44482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1d368c7494b8f6140cf0fc0b73c7e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c870fc3ee426d1a05e6d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4e6d6df7f5015f31171",
+    "url": "/static/js/2.6ca5e97f.chunk.js"
+  },
+  {
+    "revision": "d39744b49cd97a6a5d7904677be33b38",
+    "url": "/static/js/2.6ca5e97f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c870fc3ee426d1a05e6d",
+    "url": "/static/js/main.88895f7c.chunk.js"
+  },
+  {
+    "revision": "2258e4d27304b0385dd1",
+    "url": "/static/js/runtime-main.138e4bbf.js"
+  },
+  {
+    "revision": "baeadfc2ee41dc94d2110744879bc163",
+    "url": "/static/media/bonnie.baeadfc2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
